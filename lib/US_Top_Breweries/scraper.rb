096c818d4a612527ef4d6bf57663c48cf75b1d39
Thomas Pope <tpope0928@gmail.com>
2018-11-25 20:30:12 +0000

    scraper pt 1

--- lib/US_Top_Breweries/scraper.rb
+++ lib/US_Top_Breweries/scraper.rb
@@ -1,16 +1,16 @@
 class USTopBreweries::Scraper
 
   def get_page
-    Nokogiri::HTML(open("https://www.theworlds50best.com/list/1-50-winners"))
+    Nokogiri::HTML(open("https://untappd.com/brewery/top_rated?country_id=86"))
   end
 
-  def scrape_restaurants_index
-     self.get_page.css("div#t1-50 li")
+  def scrape_breweries_index
+     self.get_page.css("div#content")
   end
 
-  def make_restaurants
-    scrape_restaurants_index.each do |r|
-      WorldsBestRestaurants::Restaurant.new_from_index_page(r)
+  def make_breweries
+    scrape_breweries_index.each do |b|
+      USTopBreweries::Brewery.new_from_index_page(b)
     end
   end
 end
